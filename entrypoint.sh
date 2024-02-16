c5ef1272be68b8220096895a8432550954e8b5c0
Till Wegmueller <twe@triarc-labs.com>
2024-02-16 14:17:26 +0100

    Fix bad error response
    Signed-off-by: Till Wegmueller <twe@triarc-labs.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ KEEL_USERNAME=$4
 KEEL_PASSWORD=$5
 
 # make sure the following env variables are set
-[ -z "$IMAGE_TAG" ] && { echo "$IMAGE_TAG not set"; exit 1; }
-[ -z "$KEEL_WEBHOOK_URL" ] && { echo "$KEEL_WEBHOOK_URL not set"; exit 1; }
-[ -z "$KEEL_USERNAME" ] && { echo "$KEEL_USERNAME not set"; exit 1; }
-[ -z "$KEEL_PASSWORD" ] && { echo "$KEEL_PASSWORD not set"; exit 1; }
+[ -z "$IMAGE_TAG" ] && { echo "IMAGE_TAG not set"; exit 1; }
+[ -z "$KEEL_WEBHOOK_URL" ] && { echo "KEEL_WEBHOOK_URL not set"; exit 1; }
+[ -z "$KEEL_USERNAME" ] && { echo "KEEL_USERNAME not set"; exit 1; }
+[ -z "$KEEL_PASSWORD" ] && { echo "KEEL_PASSWORD not set"; exit 1; }
 
 curl --header "Content-Type: application/json" \
     --request POST \
